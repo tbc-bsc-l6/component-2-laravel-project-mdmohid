20807ab980e3c52befdd0704e85edbadf6b4c1fd
mdmohid <alammohid855@gmail.com>
2025-12-27 17:00:42 +0545

    added new column 'role' in user table for easy identification with user_role_id

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,72 +9,71 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+  /** @use HasFactory<\Database\Factories\UserFactory> */
+  use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'user_role_id'
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var list<string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'user_role_id',
+    'role' //user's role
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var list<string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
+
+  /**
+   * Get the attributes that should be cast.
+   *
+   * @return array<string, string>
+   */
+  protected function casts(): array
+  {
+    return [
+      'email_verified_at' => 'datetime',
+      'password' => 'hashed',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
-    /**
-     * Get the user role for the user.
-     */
-    public function userRole()
-    {
-        return $this->belongsTo(UserRole::class);
-    }
+  }
+  /**
+   * Get the user role for the user.
+   */
+  public function userRole()
+  {
+    return $this->belongsTo(UserRole::class);
+  }
 
 
-   
-    public function taughtModules()
-   {
-     return $this->hasMany(Module::class, 'teacher_id');
-   }
 
-   public function enrollments()
-   {
-     return $this->hasMany(Enrollment::class);
-   }
+  public function taughtModules()
+  {
+    return $this->hasMany(Module::class, 'teacher_id');
+  }
 
-   public function activeEnrollments()
-   {
-     return $this->enrollments()->whereNull('completed_at');
-   }
+  public function enrollments()
+  {
+    return $this->hasMany(Enrollment::class);
+  }
 
-   public function completedEnrollments()
-   {
-     return $this->enrollments()->whereNotNull('completed_at');
-   }
+  public function activeEnrollments()
+  {
+    return $this->enrollments()->whereNull('completed_at');
+  }
 
-
+  public function completedEnrollments()
+  {
+    return $this->enrollments()->whereNotNull('completed_at');
+  }
 }
